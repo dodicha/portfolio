2a2c64a14c52e743a754d8de892658ca6d1093e5
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-01-09 22:29:51 +0400

    first ground

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas camera={{ position: [0, 200, 300], fov: 75 }} style={{ width: '100vw', height: '100vh' }}>
+      {/* Lighting */}
+      <ambientLight intensity={3} />
+      <directionalLight position={[1, 0.75, 0.5]} intensity={3} />
+
+      {/* Rectangle Ground */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[500, 300]} />
+        <meshBasicMaterial color="#443c3c" side={2} />
+      </mesh>
+
+      {/* Camera Controls */}
+        <OrbitControls enablePan={false} enableZoom maxPolarAngle={Math.PI / 6} minPolarAngle={Math.PI / 3} />
+    </Canvas>
     </>
   )
 }
 
 export default App
+
