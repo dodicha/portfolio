61fc34cc86f5cf362aa3f94560a8925090099bf9
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-01-14 23:57:57 +0400

    I created a rough sample of the visual part.

--- weather/src/components/WeatherInfo.jsx
+++ weather/src/components/WeatherInfo.jsx
@@ -1,13 +1,53 @@
 
 
+function weatherByTime() {
+
+}
+
+
+
+
 function WeatherInfo() {
     return (
+        <div className="flex flex-col items-center">
+
         
-            <div className="bg-yellow-300">
+            <div className="w-300 bg-yellow-400 flex flex-col justify-center">
+
+                <div className="flex flex-col  p-15 bg-gray-700 items-center">
+                    <div>ICON</div>
+                    <div>CITY</div>
+                    <div>TEMP</div>
+                </div>
+
+                <div className="flex flex-row justify-between p-15">
+                    <div>HUMIDITY</div>
+                    <div>Wind Speed</div>
+                </div>
                 
             </div>
-        
-        
+            
+            <div className="flex flex-col w-maxcontent bg-blue-500">
+
+                <div className="flex flex-row ">
+                    <div>Temperature</div>
+                    <div>Icon</div>
+                </div>
+                
+                <div className="flex flex-row gap-15">
+                    <div>Monday</div>
+                    <div>Tuesday</div>
+                    <div>Wednesday</div>
+                    <div>Thursday</div>
+                    <div>Friday</div>
+                    <div>Saturday</div>
+                    <div>Sunday</div>
+
+
+                </div>
+
+            </div>
+        </div>
     
     )
 }
--- weather/tailwind.config.js
+++ weather/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '300': '300px',
+        '15': '15px',
+      },
+      borderRadius: {
+        '50': '50px'
+      },
+    },
   },
   plugins: [],
 }
